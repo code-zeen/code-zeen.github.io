7c27a118a82d67e31161f187ee451e6956e936d9
aejin <aejin.km@gmail.com>
2024-05-03 18:41:13 +0900

    add timeline data and styling

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,31 @@ function App() {
             <Contact />
             <Stack p={12} alignItems="stretch">
                 <AboutThisPage />
-                <Stack alignItems="flex-start" spacing={32}>
+                <Stack alignItems="flex-start" spacing={48}>
                     <Section title="소개">
-                        lorem ipsum
+                        <p>
+                            저는 미국 Illinois 주에서 물리학을 공부하는 도중 진로 고민을 하며 휴학을 계획했지만 코로나 사태가 발생한 후 대학으로 돌아가지 않았습니다.
+                            그 이후로 무엇을 해야할지 막막한 나날들을 보내다가 우연히 웹퍼블리싱 과정이 눈에 들어왔습니다.
+                        </p>
+
+                        <p>
+                            평소에 미술과 음악, 컴퓨터와 과학에 흥미를 가졌던 저는
+                            논리, 계산, 자동화, 그리고 심미적 요소까지 어우러진 퍼블리싱 (+코딩)을 배우며 새로운 열정을 찾을 수 있었습니다.
+                        </p>
+
+                        <p>
+                            저의 목표는 최고의 프론트엔드 개발자가 되는것입니다.
+                        </p>
+                        <hr />
+                        <p className="timeline">2019년 06 월 - 휴학 및 귀국</p>
+                        <p className="timeline">2022년 01 월 - 웹퍼블리셔 양성과정 수강</p>
+                        <p className="timeline"><span className="hidden">2022년</span> 05 월 - 웹퍼블리셔 양성과정 수료</p>
+                        <p className="timeline"><span className="hidden">2022년</span> 06 월 - 웹디자인기능사 자격증 취득</p>
+                        <p className="timeline"><span className="hidden">2022년</span> 07 월 - JavaScript 독학</p>
+                        <p className="timeline"><span className="hidden">2022년</span> 12 월 - React 독학</p>
+                        <p className="timeline">2023년 03 월 - 프론트엔드 개발자 입사</p>
+                        <p className="timeline">2024년 03 월 - 프론트엔드 개발자 퇴사</p>
+
                     </Section>
                     <Section title="프로젝트">
                         BingoCVM : 취약점 진단 솔루션
