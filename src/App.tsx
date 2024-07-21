98beb28ff5848af3be0f2d26c9a96001c40e9c5a
aejin <aejin.km@gmail.com>
2024-07-22 03:04:44 +0900

    componentize About Me

--- src/App.tsx
+++ src/App.tsx
@@ -2,49 +2,30 @@ import Contact from './components/specific/Contact.tsx'
 import Section from './components/common/Section.tsx'
 import { Container } from './components/styled/Container.ts'
 import { Stack } from './components/styled/Stack.ts'
-import AboutThisPage from './components/specific/AboutThisPage.tsx'
 import ImageSlide from "./components/styled/ImageSlide.tsx";
+import AboutMe from "./components/specific/AboutMe.tsx";
 
 function App() {
     return (
         <Container>
             <Contact />
             <Stack p={20} alignItems="stretch">
-                <AboutThisPage />
                 <Stack alignItems="flex-start" spacing={48}>
                     <Section title="소개">
-                        <p>
-                            저는 미국 Illinois 주에서 물리학을 공부하는 도중 진로 고민을 위해 휴학을 계획했지만 <br />코로나 사태가 발생한 후 대학으로 돌아가지 않았습니다.
-                        </p>
-                        <p>
-                            그 이후로 무엇을 해야할지 막막한 시간을 보내다 우연히 웹퍼블리싱 과정이 눈에 들어왔습니다.
-                        </p>
-                        <p>
-                            평소에 피아노, 디지털 아트, 컴퓨터와 과학에 흥미를 가졌던 저는 <br />논리, 계산, 자동화, 그리고 심미적 요소까지 어우러진 퍼블리싱 (+코딩)을 배우며 새로운 열정을 찾을 수 있었습니다.
-                        </p>
-                        <p>
-                            저의 목표는 언어에 구애받지 않는 최고의 프론트엔드 개발자가 되는것입니다.
-                        </p>
-                        <hr />
-                        <p className="timeline">2015년 08 월 - 대학 입학</p>
-                        <p className="timeline">2019년 06 월 - 휴학 및 귀국</p>
-                        <p className="timeline">2021년 12 월 - 웹퍼블리셔 수강</p>
-                        <p className="timeline">2022년 05 월 - 웹퍼블리셔 수료</p>
-                        <p className="timeline"><span className="hidden">2022년</span> 06 월 - 웹디자인기능사 취득</p>
-                        <p className="timeline"><span className="hidden">2022년</span> 07 월 - JavaScript 독학</p>
-                        <p className="timeline"><span className="hidden">2022년</span> 10 월 - TOEIC 990점 취득</p>
-                        <p className="timeline"><span className="hidden">2022년</span> 11 월 - React 독학</p>
-                        <p className="timeline">2023년 03 월 - 프론트엔드 입사</p>
-                        <p className="timeline">2024년 03 월 - 프론트엔드 퇴사</p>
-
+                        <AboutMe />
                     </Section>
                     <Section title="프로젝트">
+                        <Stack row alignItems="center">
+                            <button>HTML</button>
+                            <button>JavaScript</button>
+                            <button>React</button>
+                        </Stack>
                         <Stack row alignItems="center" spacing={8}>
                             <h3>1. BingoCVM 2.x</h3>
                             <span className="opaque">2023.04 ~ 2023.12</span>
                         </Stack>
                         <p>
-                            <a href="https://kbisys.com/" target="_blank">(주)케이비아이</a> (이하 KBI)의 자사 프로그램인 <a
+                            <a href="https://kbisys.com/" target="_blank">(주)케이비아이</a>의 자사 프로그램인 <a
                             href="http://bingocvm.com"
                             target="_blank">취약점 진단 솔루션 BingoCVM</a>의 유지보수와 기능추가를 풀스택 개발자 2명과 함께 진행했습니다.
                         </p>
---
+++ src/components/specific/AboutMe.tsx
@@ -0,0 +1,31 @@
+function AboutMe() {
+    return (
+        <>
+            <p>
+                저는 미국 Illinois 주에서 물리학을 공부하는 도중 진로 고민을 위해 휴학을 계획했지만 <br />코로나 사태가 발생한 후 대학으로 돌아가지 않았습니다.
+            </p>
+            <p>
+                그 이후로 무엇을 해야할지 막막한 시간을 보내다 우연히 웹퍼블리싱 과정이 눈에 들어왔습니다.
+            </p>
+            <p>
+                평소에 피아노, 디지털 아트, 컴퓨터와 과학에 흥미를 가졌던 저는 <br />논리, 계산, 자동화, 그리고 심미적 요소까지 어우러진 퍼블리싱 (+코딩)을 배우며 새로운 열정을 찾을 수 있었습니다.
+            </p>
+            <p>
+                저의 목표는 언어에 구애받지 않는 최고의 프론트엔드 개발자가 되는것입니다.
+            </p>
+            <br />
+            <p className="timeline">2015년 08 월 - 대학 입학</p>
+            <p className="timeline">2019년 06 월 - 휴학 및 귀국</p>
+            <p className="timeline">2021년 12 월 - 웹퍼블리셔 수강</p>
+            <p className="timeline">2022년 05 월 - 웹퍼블리셔 수료</p>
+            <p className="timeline"><span className="hidden">2022년</span> 06 월 - 웹디자인기능사 취득</p>
+            <p className="timeline"><span className="hidden">2022년</span> 07 월 - JavaScript 독학</p>
+            <p className="timeline"><span className="hidden">2022년</span> 10 월 - TOEIC 990점 취득</p>
+            <p className="timeline"><span className="hidden">2022년</span> 11 월 - React 독학</p>
+            <p className="timeline">2023년 03 월 - 프론트엔드 입사</p>
+            <p className="timeline">2024년 03 월 - 프론트엔드 퇴사</p>
+        </>
+    )
+}
+
+export default AboutMe
