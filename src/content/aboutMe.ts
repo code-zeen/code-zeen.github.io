22f743a4d76534be3bb605da81d8fc1f3d3401a8
code-zeen <aejin.km@gmail.com>
2026-05-01 20:26:51 +0200

    update AboutMe for EN and DE

---
+++ src/content/aboutMe.ts
@@ -0,0 +1,20 @@
+import { TranslatedText } from '@/types/enums.ts'
+
+export const aboutMe: TranslatedText = {
+  text: {
+    ko:
+      '<p>안녕하세요! 2년차 프론트엔드 개발자 김애진입니다.</p>' +
+      '<p>미국 일리노이 주에서 물리학을 전공하던 중 진로에 대한 고민으로 휴학을 계획했고, 이후 코로나 사태로 대학에 복귀하지 않게 되었습니다. 그 시기에 우연히 웹 퍼블리싱 과정을 접하면서 코딩에 흥미를 느끼고, 꾸준한 학습과 협업을 통해 성장하고 있습니다.</p>' +
+      '<p>문제 해결에 집중하는, 유연하고 책임감 있는 개발자를 지향합니다.</p>',
+    en:
+      '<p>Hi! I\'m Aejin, a Frontend Developer with around 2 years of experience.</p>' +
+      '<p>One winter, I signed up for an HTML/CSS course in my city out of curiosity. I was so fascinated by the world of web development, that I decided to pursue it further and have been learning and growing ever since.</p><br/>' +
+      '<p>My most recent work involved creating a web application that teachers can use to create, distribute, grade, and review tests in class, in 5 different subjects. The project honed my skills in user interaction, layout flexibility, optimization for touch devices, and working tightly with web designers, who hand-crafted hundreds of UI elements.</p><br/>' +
+      '<p>My goal is to create user- and developer-friendly experiences, to make everyone\'s life a little easier 😊</p>',
+    de:
+      '<p>Hallo! Ich bin Aejin, eine Frontend-Entwicklerin mit knapp zwei Jahren Erfahrung.</p>' +
+      '<p>Nachdem ich einen HTML/CSS-Kurs in Korea aus Neugierde belegt hatte, wurde ich von der Welt der Webentwicklung so beeindruckt, dass ich mich entschieden habe, sie weiter zu verfolgen. Seitdem habe ich immer weiter gelernt und mich weitergebildet.</p>' +
+      '<p>Meine letzte Anstellung umfasste die Erstellung einer Webanwendung, die Lehrer verwenden können, um Tests in fünf verschiedenen Fächern für ihre Klasse zu erstellen, zu verteilen, zu bewerten und zu überprüfen. Das Projekt hat meine Fähigkeiten in der Benutzerinteraktion, Layoutflexibilität, Optimierung für Touch-Geräte und engagiertes Zusammenarbeiten mit Webdesignern verbessert, die Hunderte von UI-Elementen händisch gestaltet haben.</p>' +
+      '<p>Mein Ziel ist es, benutzer- und entwicklerfreundliche UI-Erfahrungen zu erstellen, um das Leben für alle ein bisschen einfacher zu machen 😊</p>',
+  },
+}
