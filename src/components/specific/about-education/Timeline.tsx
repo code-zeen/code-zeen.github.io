15ad78cfbccef4e3f013851ddf50d9a3d5eaf2f7
code-zeen <aejin.km@gmail.com>
2026-05-04 19:05:05 +0200

    fix Timeline's dot layout issue

--- src/components/specific/about-education/Timeline.tsx
+++ src/components/specific/about-education/Timeline.tsx
@@ -7,8 +7,8 @@ export default function Timeline({ event }: { event: TimelineEvent }) {
   const textColor = event.isNewYear ? 'text-zinc-300' : 'text-zinc-700'
 
   return (
-    <div className="flex items-center gap-4">
-      <div className="flex justify-end gap-2 text-sm text-zinc-300 font-mono w-24">
+    <div className="flex gap-4">
+      <div className="flex justify-end flex-shrink-0 py-3 gap-2 text-sm text-zinc-300 font-mono w-24">
         <span className={textColor}>{event.date.start}</span>
         <span>{event.date.start && '-'}</span>
         <span className={textColor}>{event.date.end}</span>
@@ -17,7 +17,6 @@ export default function Timeline({ event }: { event: TimelineEvent }) {
         className="flex items-start gap-6 py-3 border-l-2 border-zinc-700 pl-6 relative group hover:border-zinc-500 transition-colors">
         <div
           className="absolute -left-[7px] top-4 w-3 h-3 bg-zinc-700 rounded-full group-hover:bg-zinc-500 transition-colors" />
-
 
         <div className="text-zinc-300">
           {event.text[language]}
